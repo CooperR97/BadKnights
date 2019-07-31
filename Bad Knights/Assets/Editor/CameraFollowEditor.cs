a316808ab627d6ca6ee57cbe51922d069fd4e0cc
Cooper Redfern <credfern002@drury.edu>
2019-07-31 15:17:07 -0500

    FirstPush

---
+++ Bad Knights/Assets/Editor/CameraFollowEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CameraFollow))]
+public class CameraFollowEditor : MonoBehaviour
+{
+
+	public override void OnInspectorGUI()
+	{
+	
+	}
+
+}
